47f040d6aad8b2d1913e7296d85df67426b4e20b
newms34 <newms3450@gmail.com>
2018-12-27 13:28:27 -0500

    cleaned up routes!

--- app.js
+++ app.js
@@ -2,16 +2,12 @@ const express = require('express'),
     app = express(),
     http = require('http'),
     server = http.Server(app),
-    io = require('socket.io')(server),
     path = require('path'),
     cookieParser = require('cookie-parser'),
     cookie = require('cookie'),
     bodyParser = require('body-parser'),
     cors = require('cors'),
     session = require('express-session'),
-    passport = require('passport'),
-    keys = require('./config/keys.js'),
-    // LocalStrategy = require('passport-local').Strategy,
     compression = require('compression');
 app.use(compression());
 app.use(cors());
@@ -22,7 +18,6 @@ const sesh = session({
 app.use(sesh);
 app.use(passport.initialize());
 app.use(passport.session());
-const passportSetup = require('./config/passport-setup');
 
 
 const mongoose = require('mongoose'),
@@ -52,7 +47,7 @@ db.once('open', function(e) {
 app.use(cookieParser('spero eam beatam esse'))
 app.use(bodyParser.urlencoded({ extended: false, limit: '50mb', parameterLimit: 1000000 }));
 app.use(bodyParser.json({ limit: '500mb' }));
-const routes = require('./routes')(io);
+const routes = require('./routes');
 app.set('view engine', 'html');
 app.set('views', path.join(__dirname, 'views'));
 app.set('io', io)
@@ -63,58 +58,7 @@ app.all('/*', function(req, res, next) {
     next();
 });
 app.use('/', routes);
-// var server = http.Server(app);
-// var io = require('socket.io')(server);
-let names = [];
-// io.use(function(socket, next) {
-//     sesh(socket.request, socket.request.res, next);
-// });
-let isFirstCon = true;
-io.on('connection', function(socket) {
-    //death stuff
-    // socket.on('disconnect',()=>{
-    //     console.log('someone left q.q')
-    //     socket.emit('reqHeartBeat',{})
-    //     names = [];
-    // })
-    if (isFirstCon) {
-        isFirstCon = false;
-        socket.emit('doLogout')
-    }
-    socket.on('hbResp', function(n) {
-        // console.log('heartbeat response from',n)
-        for (let i = 0; i < names.length; i++) {
-            if (names[i].name == n.name) {
-                names[i].t = Date.now();
-            }
-        }
-        let now = Date.now();
-        names = names.filter(nm => now - nm.t < 1000)
-    })
-
-    // //new login stuff
-    socket.on('hiIm', function(n) {
-        //on a new person connecting, add them to our list and then push out the list of all names.
-        names.push({ name: n.name, t: Date.now() });
-        console.log('NEW USER', n, 'ALL USERS', names)
-        // socket.emit('allNames',names);
-    })
-
-    socket.on('getOnline', function() {
-        socket.emit('allNames', names);
-    })
-    setInterval(function() {
-        socket.emit('reqHeartBeat', {});
-        socket.emit('allNames', names)
-    }, 500);
-
-    //messaging (for chat!)
-    socket.on('chatMsg', function(msgObj) {
-        console.log('chat message sent! Obj was', msgObj)
-        msgObj.time = Date.now();
-        io.emit('msgOut', msgObj)
-    })
-});
+//if we're on heroku, they set their own port, so we get that from the PORT environment variable
 server.listen(process.env.PORT || 8080);
 server.on('error', function(err) {
     console.log('Oh no! Err:', err)
